6bd35b914cad62e377b15176aec80bc60c373fcb
Eduardo Azevedo <eduardoazevedo3@gmail.com>
2025-07-17 23:13:10 -0300

    fix: update account validations to ensure uniqueness of document_number scoped by document_type and provider_account_id

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,12 +2,12 @@ class Account < ApplicationRecord
   DOCUMENT_TYPE_ENUM = %w[ CPF CNPJ ].freeze
 
   validates :provider_account_id, uniqueness: true
-  validates :provider_account_id, uniqueness: { scope: %i[ document_type document_number ] }
   validates :provider_account_id, presence: true, length: { maximum: 64 }, format: { with: REGEX_ALPHANUMERIC_WITH_DOTS }
   validates :reference_code, allow_nil: true, length: { maximum: 64 }, format: { with: REGEX_ALPHANUMERIC_WITH_DOTS }
   validates :description, presence: true, length: { in: 3..255 }, format: { with: REGEX_BUSINESS_NAME }
   validates :document_type, presence: true, length: { maximum: 10 }, inclusion: { in: DOCUMENT_TYPE_ENUM }
   validates :document_number, presence: true, length: { in: 14..18 }, format: { with: REGEX_DOCUMENTATION }
+  validates :document_number, uniqueness: { scope: %i[ document_type provider_account_id ] }
 
   squishize :description, :document_number
 
