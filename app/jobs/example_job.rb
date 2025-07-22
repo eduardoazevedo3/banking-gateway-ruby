7973e42098ed1a35d1614d5bcce045114ae226de
Eduardo Azevedo <eduardoazevedo3@gmail.com>
2025-07-22 20:13:55 -0300

    fix: rubocop offenses

--- app/jobs/example_job.rb
+++ app/jobs/example_job.rb
@@ -2,8 +2,7 @@ class ExampleJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    Rails.logger.info "ExampleJob executado com argumentos: #{args.inspect}"
-
-    Rails.logger.info "ExampleJob finalizado com sucesso"
+    # Execute something later
+    puts 'Running example job'
   end
 end
--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,4 @@
-ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-require "bundler/setup" # Set up gems listed in the Gemfile.
-require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+require 'bundler/setup' # Configura as gems listadas no Gemfile.
+require 'bootsnap/setup' # Acelera o boot usando cache para carregar mais rápido.
