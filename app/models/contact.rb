a638b79b3d6eceb50f5325d2ec78982b44b47f42
Eduardo Azevedo <eduardoazevedo3@gmail.com>
2025-08-09 23:25:03 -0300

    feat: add document validation for CPF and CNPJ in contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,6 @@
 class Contact < ApplicationRecord
+  include DocumentValidate
+
   DOCUMENT_TYPES = %w[ CPF CNPJ ].freeze
 
   belongs_to :account
@@ -6,6 +8,8 @@ class Contact < ApplicationRecord
   validates :full_name, length: { in: 3..255 }
   validates :document_type, inclusion: { in: DOCUMENT_TYPES }
   validates :document_number, format: { with: REGEX_DOCUMENTATION }
+  validates :document_number, length: { is: 14 }, if: -> { document_type == 'CPF' }
+  validates :document_number, length: { is: 18 }, if: -> { document_type == 'CNPJ' }
   validates :phone, length: { in: 14..15 }, format: { with: REGEX_PHONE_NUMBER }
   validates :zip_code, length: { is: 9 }
   validates :address, length: { in: 3..255 }
@@ -13,6 +17,8 @@ class Contact < ApplicationRecord
   validates :city, length: { in: 3..255 }
   validates :neighborhood, length: { in: 3..255 }
   validates :state, length: { is: 2 }, format: { with: REGEX_STATE_CODE }
+
+  document_validate :document_number
 
   add_scope :by_reference_code do |reference_code|
     where(reference_code:) if reference_code.present?
